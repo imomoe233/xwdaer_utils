49aa79808ce4313b40ff51d538163abb3eece508
Xu Weida <imomoe233@outlook.com>
2023-02-20 16:21:15 +0800

    update

--- grad_cam_cifar10.py
+++ grad_cam_cifar10.py
@@ -23,7 +23,6 @@ from torchvision import datasets, transforms
 from collections import defaultdict
 from torch.utils.data import DataLoader, random_split, TensorDataset
 import pickle
-from gradcam import *
 from gradcam import GradCAM, GradCAMpp
 from gradcam.utils import visualize_cam
 import PIL
@@ -41,12 +40,12 @@ label_dict = {
     8:'ship',
     9:'truck'
 }
- 
+
 def main():
     model = ResNet18(10)
     model.cuda()
 
-    params = torch.load("D:\code\code_xwd\Durable-Federated-Learning-Backdoor\SAVE_MODEL\cifar10 patched 90%敌手\Backdoor_saved_models_update1_noniid_EC0_cifar10_Baseline_EE3801\\target_model_epoch_1870.pth")
+    params = torch.load("F:\SAVE_MODEL\cifar10 patched 90%敌手\Backdoor_saved_models_update1_noniid_EC0_cifar10_Baseline_EE3801\\target_model_epoch_1870.pth")
     model.load_state_dict(params)
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.2,
@@ -55,17 +54,14 @@ def main():
     
     gradcam = GradCAM.from_config(model_type='resnet', arch=model, layer_name='layer4')
     for k in range (0, 11):
-        
         # get an image and normalize with mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
-        pil_img = PIL.Image.open(f'D:\code\code_xwd\dataset\patched-cifar-10\pic\{k}.jpg')
+        pil_img = PIL.Image.open(f'F:\patched-cifar-10\\test_pic\{k}.png')
         torch_img = transforms.Compose([transforms.Resize((32, 32)), transforms.ToTensor()])(pil_img).cuda()
         normed_img = transforms.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010])(torch_img)[None]
         # get a GradCAM saliency map on the class index 10.
         output = model(normed_img)
         pred = output.data.max(1)[1]
         print(pred)
-        
-        
         
         plt.figure()
         for i in range(0, 10):
@@ -74,7 +70,7 @@ def main():
             # make heatmap from mask and synthesize saliency map using heatmap and img
             heatmap, cam_result = visualize_cam(mask, torch_img)
         
-            
+        
             # plt.subplot(1,2,1)
             
             # plt.imshow(transforms.ToPILImage()(heatmap))
--- models/resnet.py
+++ models/resnet.py
@@ -13,13 +13,16 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
+        # self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
+        # self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                # nn.BatchNorm2d(self.expansion*planes)
                 nn.BatchNorm2d(self.expansion*planes)
             )
 
@@ -66,17 +69,18 @@ class ResNet(SimpleNet):
         super(ResNet, self).__init__()
         self.in_planes = 32
 
-        # self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        # self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
+        # self.bn1 = nn.BatchNorm2d(32)
         self.layer1 = self._make_layer(block, 32, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 64, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 128, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 256, num_blocks[3], stride=2)
-        self.layer5 = self._make_layer(block, 512, num_blocks[2], stride=2)
-        self.layer6 = self._make_layer(block, 1024, num_blocks[3], stride=2)
-        #self.linear = nn.Linear(256*block.expansion, num_classes)
-        self.linear = nn.Linear(1024*block.expansion, num_classes)
+        #self.layer5 = self._make_layer(block, 512, num_blocks[2], stride=2)
+        #self.layer6 = self._make_layer(block, 1024, num_blocks[3], stride=2)
+        self.linear = nn.Linear(256*block.expansion, num_classes)
+        # self.linear = nn.Linear(1024*block.expansion, num_classes)
 
         #########################################################################################
         # 这里的模型需要改，在cifar10 下，模型就4层，到256结束，但在FMNIST 下，模型6层到1024结束         #
@@ -98,15 +102,15 @@ class ResNet(SimpleNet):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        #out = F.avg_pool2d(out, 4)
-        out = F.avg_pool2d(out, 2)
+        out = F.avg_pool2d(out, 4)
+        #out = F.avg_pool2d(out, 2)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
         #######################################################################################################
-        # 这里的模型需要改，在cifar10 下，out = F.avg_pool2d(out, 4)，但在FMNIST 下，out = F.avg_pool2d(out, 2)  #
+        # 这里的模型需要改，在cifar10 下，out = F.avg_pool2d(out, 4)，但在FMNIST 下，out = F.avg_pool2d(out, 2)    #
         #######################################################################################################
 
 
